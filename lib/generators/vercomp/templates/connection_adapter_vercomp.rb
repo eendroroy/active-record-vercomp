5d2f1f851acf15afce4123d33487cf69e6c2db2b
indrajit <eendroroy@gmail.com>
2018-09-03 11:13:56 +0600

    create generator

--- lib/generators/vercomp/templates/connection_adapter_vercomp.rb
+++ lib/generators/vercomp/templates/connection_adapter_vercomp.rb
@@ -1,11 +1,21 @@
+# frozen_string_literal: true
+
 module ActiveRecord
   module ConnectionAdapters
     if const_defined?(:PostgreSQLAdapter)
       class PostgreSQLAdapter
         NATIVE_DATABASE_TYPES.merge!(
-            address_status:  { name: 'character varying' },
-            address_type:    { name: 'character varying' }
+          version: { name: 'VERSION' }
         )
+      end
+    end
+    if const_defined?(:PostgreSQLAdapter)
+      module PostgreSQL
+        module ColumnMethods
+          def version(*args, **options)
+            args.each { |name| column(name, :version, options) }
+          end
+        end
       end
     end
   end
--- lib/generators/vercomp/vercomp_generator.rb
+++ lib/generators/vercomp/vercomp_generator.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class VercompGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
+  source_root File.expand_path('templates', __dir__)
   def copy_initializer
     copy_file '../templates/connection_adapter_vercomp.rb', 'config/initializers/connection_adapter_vercomp.rb'
   end
